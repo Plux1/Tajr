659aa0190098c1375f4282e24bcccc20dd78d564
Plux1 <issajuma11@gmail.com>
2020-11-07 11:10:51 +0300

    Setting up frontend and testing

--- booktime/main/tests.py
+++ booktime/main/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
+#The reverse() method imported here allow to call url path via their names
 # We are writing a test focusing on testing the behaviour at the HTTP level
 
 class TestPage(TestCase):
@@ -8,7 +10,17 @@ class TestPage(TestCase):
     # the template home.html has been used
     # response contains the name of our shop
     def test_home_page_works(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
+    
+    # About us page test case
+    # Http status code = 200
+    # The template about_us.html has been used
+    # Response contains the name of our shop (BookTime)
+    def test_about_us_page(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
