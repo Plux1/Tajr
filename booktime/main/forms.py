0da3e42e62b362d9b8fb1c970086bd4aa64de8f2
Plux1 <issajuma11@gmail.com>
2020-11-10 14:01:19 +0300

    Separate test file into test_views and test_forms

---
+++ booktime/main/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.core.mail import send_mail
+
+class ContactForm(forms.Form):
+    name = forms.Charfield(label='Your name', max_length=100)
+    message = forms.CharField(max_length=600, widget=forms.Textarea )
+
+    # Method to send emails
+    def send_mail(self):
+        message = "From: {0}\n{0}".format(self.cleaned_data['name'], self.cleaned_data['message'])
+        send_mail("Site message", message, "site@booktime.domain", ["customerservice@booktime.domain"], fail_silently=False)
