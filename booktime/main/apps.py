32f981d827582f07289eba29ac69c4a4e1735659
Plux1 <issajuma11@gmail.com>
2020-11-12 12:33:30 +0300

    Working with database

--- booktime/main/apps.py
+++ booktime/main/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    # initialize signals.py file when the django application is launched 
+    # by the internal django application registry
+    def read(self):
+        from . import signals
